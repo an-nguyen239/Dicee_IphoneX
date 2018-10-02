531f51343313b839fafd37eeb976888660990765
An Nguyen <anguyen108@student.gsu.edu>
2018-10-01 21:45:51 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by An Nguyen on 10/1/18.
+//  Copyright © 2018 An Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
